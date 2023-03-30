e97e6cbc470f413c9eaa153a309f4ae881a1f653
dongyeon.kim <dongyeon.kim@lasee.io>
2023-03-31 08:51:24 +0900

    컴포넌트 스타일 구성하기

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import TodoTemplate from './TodoTemplate';
 import TodoInsert from './TodoInsert';
+import TodoList from './TodoList';
 
 function App() {
     return (
         <TodoTemplate>
             <TodoInsert />
+            <TodoList />
         </TodoTemplate>
     );
 }
---
+++ src/TodoList.js
@@ -0,0 +1,15 @@
+import TotoListItem from './TodoListItem';
+import './TodoList.scss';
+import TodoListItem from './TodoListItem';
+
+const TodoList = () => {
+    return (
+        <div className="TodoList">
+            <TodoListItem />
+            <TodoListItem />
+            <TodoListItem />
+        </div>
+    );
+};
+
+export default TodoList;
